9bf7ce76e4b1688874ec7797f69b8d8a319d6dcd
Mehmet Erkun <mehmetterkunn@gmail.com>
2026-05-16 18:14:09 +0300

    4x4 matriks izgarasi olusturuldu ve renkleri ayarlandı

--- main.cpp
+++ main.cpp
@@ -1,61 +1,70 @@
 #include <SFML/Graphics.hpp>
 
 int main() {
-    // asagidaki kod pencere olusturuyor
-    sf::RenderWindow pencere(sf::VideoMode(800, 800), "2048 - Ana Proje", sf::Style::Default);
+    // 2048 adli pencereyi olusturuyor
+    sf::RenderWindow pencere(sf::VideoMode(800, 800), "2048", sf::Style::Default);
 
-    
     const float OYUN_GENISLIK = 800.0f;
     const float OYUN_YUKSEKLIK = 800.0f;
-
-    // kare formunu koruyor
     sf::View gorunum(sf::FloatRect(0, 0, OYUN_GENISLIK, OYUN_YUKSEKLIK));
+
+    // --- IZGARA TASARIM AYARLARI ---
+    const float IZGARA_BOYUTU = 580.0f;
+    const float IZGARA_X = 110.0f;
+    const float IZGARA_Y = 110.0f;
+    
+    const float KUTU_BOYUTU = 120.0f;
+    const float BOSLUK = 20.0f;
+
+    sf::RectangleShape arkaPlanIzgara(sf::Vector2f(IZGARA_BOYUTU, IZGARA_BOYUTU));
+    arkaPlanIzgara.setFillColor(sf::Color(187, 173, 160)); 
+    arkaPlanIzgara.setPosition(IZGARA_X, IZGARA_Y);
+
+    sf::RectangleShape bosKutu(sf::Vector2f(KUTU_BOYUTU, KUTU_BOYUTU));
+    bosKutu.setFillColor(sf::Color(205, 193, 180)); 
 
     while (pencere.isOpen()) {
         sf::Event olay;
-        while (pencere.pollEvent(olay)){
+        while (pencere.pollEvent(olay)) {
             if (olay.type == sf::Event::Closed)
                 pencere.close();
 
-            // tam ekranda ölçüleri koruyo
-            if(olay.type == sf::Event::Resized){
+            if (olay.type == sf::Event::Resized) {
                 float yeniGenislik = static_cast<float>(olay.size.width);
                 float yeniYukseklik = static_cast<float>(olay.size.height);
-
                 float ekranOrani = yeniGenislik / yeniYukseklik;
                 float oyunOrani = OYUN_GENISLIK / OYUN_YUKSEKLIK;
-
                 float gorunumX = 0.0f, gorunumY = 0.0f;
                 float gorunumGenislik = 1.0f, gorunumYukseklik = 1.0f;
 
-                //oyun ekrani disindaki yerleri siyah yap
-                if(ekranOrani > oyunOrani){
+                if (ekranOrani > oyunOrani) {
                     gorunumGenislik = oyunOrani / ekranOrani;
                     gorunumX = (1.0f - gorunumGenislik) / 2.0f;
-                }
-                else{
+                } else {
                     gorunumYukseklik = ekranOrani / oyunOrani;
                     gorunumY = (1.0f - gorunumYukseklik) / 2.0f;
                 }
-
-                // Hesaplanan yeni oranlari gorunume sabitliyoruz
                 gorunum.setViewport(sf::FloatRect(gorunumX, gorunumY, gorunumGenislik, gorunumYukseklik));
             }
         }
 
-        pencere.clear(sf::Color::Black); // Arka plan siyah
-
-        // Ayarladigimiz akilli gorunumu pencereye uyguluyoruz
+        pencere.clear(sf::Color(243,239,227));
         pencere.setView(gorunum);
 
-        // Ortadaki test karemizi tekrar ciziyoruz
-        sf::RectangleShape testKaresi(sf::Vector2f(400.0f, 400.0f));
-        testKaresi.setFillColor(sf::Color::Green);
-        testKaresi.setPosition(200.0f, 200.0f); 
-        
-        pencere.draw(testKaresi);
+        pencere.draw(arkaPlanIzgara);
+
+        for (int satir = 0; satir < 4; satir++) {
+            for (int sutun = 0; sutun < 4; sutun++) {
+                float kutuX = IZGARA_X + BOSLUK + sutun * (KUTU_BOYUTU + BOSLUK);
+                float kutuY = IZGARA_Y + BOSLUK + satir * (KUTU_BOYUTU + BOSLUK);
+                
+                bosKutu.setPosition(kutuX, kutuY);
+                pencere.draw(bosKutu);
+            }
+        }
 
         pencere.display();
     }
+
     return 0;
 }
