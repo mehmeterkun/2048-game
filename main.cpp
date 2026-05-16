dd05447ee0282d63b4cb28fd9dfdff0b81fbacc1
Mehmet Erkun <mehmetterkunn@gmail.com>
2026-05-16 16:57:41 +0300

    Ekranin ortasina yeşil kare bir kutu olusturuldu lakin tam ekran yapinca kare bozuluyor

--- main.cpp
+++ main.cpp
@@ -1,16 +1,26 @@
 #include <SFML/Graphics.hpp>
 
 int main() {
-    // siyah bir ekran oluşacak
-    sf::RenderWindow window(sf::VideoMode(800, 800), "2048 ");
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
-                window.close();
+    // asagıdaki kod pencere olusturuyo
+    sf::RenderWindow pencere(sf::VideoMode(800, 800), "2048", sf::Style::Default);
+
+    while (pencere.isOpen()){
+        sf::Event olay;
+        while (pencere.pollEvent(olay)){
+            if (olay.type == sf::Event::Closed)
+                pencere.close();
         }
-        window.clear(sf::Color::Black);
-        window.display();
+
+        pencere.clear(sf::Color::Black); // Arka plan siyah
+
+        // yesil bir kare cizdiriyor
+        sf::RectangleShape testKaresi(sf::Vector2f(400.0f, 400.0f));
+        testKaresi.setFillColor(sf::Color::Green);
+        testKaresi.setPosition(200.0f, 200.0f); // orta
+        
+        pencere.draw(testKaresi);
+
+        pencere.display();
     }
     return 0;
 }
